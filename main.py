e017deba0794b3632b4fb07d3e023b7a58ba1014
yunjae-teacher <hernarcis@gmail.com>
2025-08-06 22:03:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -18,4 +19,5 @@ def home():
     return "Byte Counter API is running."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
