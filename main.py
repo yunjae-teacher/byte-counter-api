d903bcf9911baed8b2700dddf5334b86d3c13bb2
yunjae-teacher <hernarcis@gmail.com>
2025-08-17 13:59:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,6 @@ def calculate_byte_count():
 def home():
     return "Byte Counter API is running."
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+#if __name__ == "__main__":
+#   port = int(os.environ.get("PORT", 5000))
+#    app.run(host="0.0.0.0", port=port)
